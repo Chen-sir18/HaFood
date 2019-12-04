f224d6f3d03d329611efd38c5fb0ab5082584afa
757363985 <757363985@qq.com>
2019-12-04 14:10:39 +0800

    请下载这个文件夹，其它的文件作为初始备份

---
+++ HaFood/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+  <input type="checkbox" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ HaFood/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import store from './store/index.js'
+import router from './router'
+import './less/public.less'
+import './less/reset.less'
+import './font/iconfont.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
