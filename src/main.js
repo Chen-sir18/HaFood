973737c1a23c360bfab89f7189c9e8014f480732
757363985 <757363985@qq.com>
2019-12-04 14:06:41 +0800

    这是加入element-ui框架之后

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import router from './router'
 import './less/public.less'
 import './less/reset.less'
 import './font/iconfont.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
