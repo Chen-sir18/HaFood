ba11f89e42005cf66ea00dfeb9ebebce25929662
757363985 <757363985@qq.com>
2019-12-10 18:48:05 +0800

    最新版本

--- HaFood/src/router/index.js
+++ HaFood/src/router/index.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import Home1 from '../components/home/Home1.vue'
 import Home2 from '../components/home/Home2.vue'
 import Login from '../components/public/login.vue'
+import Blogdetail from '../components/blogs/blogdetail.vue'
+import Bloggrid from '../components/blogs/bloggrid.vue'
+import Blogmasonry from '../components/blogs/blogmasonry.vue'
+import Bloglist from '../components/blogs/blogsbody.vue'
 import App from '../App.vue'
 Vue.use(Router)
 
@@ -24,7 +28,23 @@ export default new Router({
         {
         	path: '/Home2',
       		component: Home2
-        }
+        },
+        {
+        	path: '/BlogList',
+        	component: Bloglist
+        },
+        {	
+        	path: '/BlogGrid',
+        	component: Bloggrid
+       	},
+       	{
+       		path: '/BlogMasonry',
+        	component: Blogmasonry
+       	},
+       	{
+       		path: '/BlogDetails',
+        	component: Blogdetail
+       	}
       ]
     },
     {
