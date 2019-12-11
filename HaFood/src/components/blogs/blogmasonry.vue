8422909752094ce5c7a318ab86f7fc62975b8cb7
757363985 <757363985@qq.com>
2019-12-11 19:21:36 +0800

    第二次上传

--- HaFood/src/components/blogs/blogmasonry.vue
+++ HaFood/src/components/blogs/blogmasonry.vue
@@ -4,7 +4,7 @@
     <section class="h-blog-grid" style="min-height: 800px">
       <div class="h-menu-outer">
         <ul>
-          <li :key="index" v-for="(item, index) in tabs" @click="styleClick(index, item.view)" :class="{active:active==index}" class="h-list-menu">{{item.title}}</li>
+          <li :key="index" v-for="(item, index) in tabs" @click="styleClick(index)" :class="{active:active==index}" class="h-list-menu">{{item.title}}</li>
           <!-- <li @click="styleClick" :class="{active:active==index}" class="h-list-menu">Restaurant</li>
           <li :key="index" v-for="{tab, index} in tabs" @click="styleClick(index)" @click="styleClick" class="h-list-menu">Bakery</li>
           <li @click="styleClick" class="h-list-menu">Salads</li> -->
@@ -103,7 +103,7 @@ export default {
         url: 'api/cakes',
         method: 'get',
         params: {
-          // æ­¤å¤„ä¸ä¼ å‚
+          // ´Ë´¦²»´«²Î
         }
       }).then((res) => {
         console.log(res.data)
@@ -122,7 +122,7 @@ export default {
         url: 'api/foodclass',
         method: 'get',
         params: {
-          // æ­¤å¤„ä¸ä¼ å‚
+          // ´Ë´¦²»´«²Î
         }
       }).then((res) => {
         console.log(res.data)
@@ -141,7 +141,7 @@ export default {
       url: 'api/popular',
       method: 'get',
       params: {
-        // æ­¤å¤„ä¸ä¼ å‚
+        // ´Ë´¦²»´«²Î
       }
     }).then((res) => {
       console.log(res.data)
