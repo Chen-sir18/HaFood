77ab0c589a0b73ea4fc3ddacd8f79ddbdeeb5a20
hujjing <1164293296@qq.com>
2019-12-20 19:21:01 +0800

    hujing 12-20 19:20

--- HaFood/src/components/Product/Common/Prdetrigger.vue
+++ HaFood/src/components/Product/Common/Prdetrigger.vue
@@ -51,18 +51,21 @@ export default {
 
 <style scoped lang="less">
   .x-pr-detrigger{
-    height: 606px;
+    width: 100%;
+    // height: 606px;
     background: #f6f6f6;
     padding: 83px 0 88px;
     margin: 0 0 83px;
     .x-pr-trigger{
-      height: 606px;
+      // height: 606px;
       width: 1140px;
       margin: 0 auto;
       ul:nth-child(1){
+        width: 100%;
+        padding: 0 15px;
         display: flex;
         justify-content: center;
-        height: 42px;
+        // height: 42px;
         margin: 0 0 60px;
         border-bottom: 2px solid #ddd;
         li{
@@ -78,23 +81,110 @@ export default {
       }
       ul:nth-child(2){
         display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        align-items: center;
         justify-content: center;
         li{
-          width: 570px;
-          height: 330px;
+          width: 50%;
           padding: 0 15px;
+          // flex: 0 0 50%;
           overflow: hidden;
           img{
-            width: 540px;
-            height: 330px;
+            width: 100%;
+            vertical-align: middle;
           }
         }
         li:nth-child(2){
-          padding: 50px 15px;
+          width: 50%;
+          // padding: 0 15px;
+          flex: 0 0 50%;
+          padding: 0px 15px;
           p{
+            width: 100%;
             line-height: 30px;
             font-size: 14px;
             color: #555555;
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 992px) and (max-width: 1200px) {
+    .x-pr-detrigger{
+      width: 100%;
+      .x-pr-trigger{
+        width: 960px;
+        margin: 0 auto;
+        ul:nth-child(1){
+          width: 100%;
+        }
+      }
+    }
+  }
+  @media (min-width: 767px) and (max-width: 992px) {
+    .x-pr-detrigger{
+      width: 100%;
+      .x-pr-trigger{
+        width: 720px;
+        margin: 0 auto;
+        ul:nth-child(1){
+          width: 100%;
+        }
+      }
+    }
+  }
+  @media (min-width: 567px) and (max-width: 767px) {
+    .x-pr-detrigger{
+      width: 100%;
+      .x-pr-trigger{
+        width: 540px;
+        margin: 0 auto;
+        ul:nth-child(1){
+          width: 100%;
+        }
+        ul:nth-child(2){
+          width: 100%;
+          // align-items:
+          li{
+            width: 100%;
+          }
+          li:nth-child(2){
+            width: 100%;
+            flex: 0 0 100%;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            p{
+              width: 100%;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 567px) {
+    .x-pr-detrigger{
+      width: 100%;
+      .x-pr-trigger{
+        width: 100%;
+        margin: 0 auto;
+        ul:nth-child(1){
+          width: 100%;
+        }
+        ul:nth-child(2){
+          width: 100%;
+          // align-items:
+          li{
+            width: 100%;
+          }
+          li:nth-child(2){
+            width: 100%;
+            flex: 0 0 100%;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            p{
+              width: 100%;
+            }
           }
         }
       }
